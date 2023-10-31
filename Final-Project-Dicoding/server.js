fa71002ed85def5c5a6267b9b56928f8cfaab568
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2023-10-31 14:22:09 +0700

    New file

---
+++ Final-Project-Dicoding/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 5001;
+
+app.use('/', express.static(path.join(__dirname, '/')));
+
+app.listen(PORT, function (err) {
+    if (err) console.log("Error in server setup");
+    console.log(`Server berjalan di http://localhost:${PORT}`) 
+});
