496f4377bd88745d4e6f9c516a1f8736adecf12f
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2023-11-21 21:14:54 +0700

    Update app.js

--- Final-Project-Dicoding/js/app.js
+++ Final-Project-Dicoding/js/app.js
@@ -11,10 +11,6 @@ const days = [
   "Friday",
   "Saturday",
 ]; // days
-const title = document.getElementById("inputBookTitle").value;
-const author = document.getElementById("inputBookAuthor").value;
-const year = document.getElementById("inputBookYear").value;
-const isComplete = document.getElementById("inputBookIsComplete").checked;
 
 // Function Time
 let time = setInterval(updateTime, 1000);
@@ -27,80 +23,59 @@ function updateTime() {
   document.getElementById("clock").innerText = `${days[dayNow]} ${hours}:${minutes}:${seconds}`;
 }
 
-// Function initialization session storage
-function init (title, author, year, isComplete) {
-  if (sessionStorage.getItem(title) === null) {
-    sessionStorage.setItem(title, "");
-  }
-  if (sessionStorage.getItem(author) === null) {
-    sessionStorage.setItem("author", "");
-  }
-  if (sessionStorage.getItem(year) === null) {
-    sessionStorage.setItem("year", 0);
-  }
-  if (sessionStorage.getItem(isComplete) === null) {
-    sessionStorage.setItem("isComplete", "false");
-  }
+// Function initialize 
+function init () {
+  if(sessionStorage.getItem("key") === null ) { sessionStorage.setItem("key", 0); }
+  if(sessionStorage.getItem("title") === null ) { sessionStorage.setItem("title", ""); }
+  if(sessionStorage.getItem("author") === null ) { sessionStorage.setItem("author", ""); }
+  if(sessionStorage.getItem("year") === null ) { sessionStorage.setItem("year", 0); }
+  if(sessionStorage.getItem("isComplete") === null ) { sessionStorage.setItem("isComplete", false); }
 }
 
-// Function to check whether the book has been read or not
-function checkBook () {
-  if (sessionStorage.getItem('isCompleted') === "true") {
-    incompleteBookshelfList.removeAttribute('hidden');
-    let incompleteBookshelfList = document.getElementById('incompleteBookshelfList');
-    let titleIncompleteList = document.querySelector('titleIncompleteList');
-    let authorIncompleteList = document.querySelector('authorIncompleteList');
-    let yearIncompleteList = document.querySelector('yearIncompleteList');
-    titleIncompleteList.innerText = sessionStorage.getItem('title');
-    authorIncompleteList.innerText = "Penulis:" + sessionStorage.getItem('author');
-    yearIncompleteList.innerText = "Tahun: " + sessionStorage.getItem('year');
-  }
-  if (sessionStorage.getItem('isCompleted') === "false") {
+// function setItem
+function addValue () {
+  let title = document.getElementById("inputBookTitle").value;
+  let author = document.getElementById("inputBookAuthor").value;
+  let year = document.getElementById("inputBookYear").value;
+  let isComplete = document.getElementById("inputBookIsComplete").checked;
+  let key = +new Date();
 
-  }
+  sessionStorage.setItem("key", key); 
+  sessionStorage.setItem("title", title);
+  sessionStorage.setItem("author", author); 
+  sessionStorage.setItem("year", year); 
+  sessionStorage.setItem("isComplete", isComplete);
 }
 
+// Function incomplete read
+function inCompleteRead () {
+  let inCompleteTitle = document.querySelector("titleIncompleteList");
+  let inCompleteAuthor = document.querySelector("authorIncompleteList");
+  let inCompleteYear = document.querySelector("yearIncompleteList");
+  inCompleteTitle.innerText = sessionStorage.getItem("title");
+  inCompleteAuthor.innerText = `Author: ${sessionStorage.getItem("author")}`;
+  inCompleteYear.innerText = `Year: ${sessionStorage.getItem("year")}`;    
+}
 
+// Function complete read
+function completeRead () {
+  let completeTitle = document.querySelector("titleCompleteList");
+  let completeAuthor = document.querySelector("authorCompleteList");
+  let completeYear = document.querySelector("yearCompleteList");
+  completeTitle.innerText = sessionStorage.getItem("title");
+  completeAuthor.innerText = `Author: ${sessionStorage.getItem("author")}`;
+  completeYear.innerText = `Year: ${sessionStorage.getItem("year")}`;     
+}
 
-// Function unfinished read
-// Function edit book
-function editIncomplteBookShelfList () {
-  let editIncomplete = document.getElementById('editIncompleteBookshelfList');
-  let incompleteBookshelfList = document.getElementById('incompleteBookshelfList')
+// Initialize 
+window.addEventListener('load', function () {
+  typeof(Storage) !== "undefined" ? init() : console.log(`Browser tidak support`);
+})
 
-  editButtonIncomplete.addEventListener('click', function () {
-    if (editIncomplete.hasAttribute('hidden')) {
-      editIncomplete.removeAttribute('hidden');
-      incompleteBookshelfList.setAttribute('hidden', 'true');
-    }
-  })
-} // End function
-
-// Initialization session storage
-window.addEventListener("load", function () {
-  if (typeof Storage !== "undefined") {
-    init(title, author, year, isComplete);
-  } else {
-    alert("Browser does not support session storage.");
-  }
-}); // End function
-
-// Event button submit
-buttonBookSubmit.addEventListener("click", function () {
-  let key = +new Date().getTime();
-  if(sessionStorage.getItem(key) ===  )
-    // sessionStorage.setItem("key", key);
-    // sessionStorage.setItem("title", title);
-    // sessionStorage.setItem("author", author);
-    // sessionStorage.setItem("year", year);
-    //   if (isComplete === true) {
-    //     sessionStorage.setItem("isCompleted", "true");
-    //   } else {
-    //     sessionStorage.setItem("isCompleted", "false");
-    //   }
-}); // Event submit button end
-
-// Event button edit incomplete
-editButtonIncomplete.addEventListener('click', function () {
-  editIncomplteBookShelfList();
+// Button submit
+buttonBookSubmit.addEventListener('click', function () {
+  addValue();
+  sessionStorage.getItem("isComplete") == false ? 
+  (document.querySelector("inCompleteHidden").removeAttribute("hidden"), inCompleteRead()):
+  (document.querySelector("completeHidden").removeAttribute("hidden"), completeRead());
 })
--- Final-Project-Dicoding/server.js
+++ Final-Project-Dicoding/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 5050;
+const PORT = 8000;
 
 app.use('/', express.static(path.join(__dirname, '/')));
 
